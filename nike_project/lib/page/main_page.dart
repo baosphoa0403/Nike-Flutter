56e38e6adcf54c268fd834d140c30f61c604111d
baosphoa0403 <baosphoa0403@gmail.com>
2021-11-06 09:13:12 +0700

    Layout Sign In NK-5

--- nike_project/lib/page/main_page.dart
+++ nike_project/lib/page/main_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:nike_project/page/SignUp/sign_up_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("MainPage"),
-      ),
+    return const Scaffold(
+      body: SignUpPage(),
     );
   }
 }
