6e03f0c8008966e77c7bab7de8b6701dc1ff026f
DQLok <wkhhkw01@gmail.com>
2021-11-09 20:43:57 +0700

    change file

--- nike_project/lib/page/sign_up_page/component/screen.dart
+++ nike_project/lib/page/sign_up_page/component/screen.dart
@@ -3,7 +3,7 @@ import 'package:nike_project/common/Button/elevated_button_custom.dart';
 import 'package:nike_project/common/Button/outlined_button_customer.dart';
 import 'package:nike_project/contants/contants.dart';
 import 'package:nike_project/page/sign_up_page/component/backgroud.dart';
-import 'package:nike_project/page/sign_up_page/component/body/body1.dart';
+import 'package:nike_project/page/sign_up_page/component/body/body.dart';
 import 'package:nike_project/page/sign_up_page/component/footer/footer.dart';
 import 'package:nike_project/page/sign_up_page/component/header/header.dart';
 import 'package:nike_project/page/sign_up_page/contants/contans.dart';
